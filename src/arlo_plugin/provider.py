6fe0fd692607c8e8a706542076031d12f10ac9ad
Brett Jia <dev.bjia56@gmail.com>
2025-02-18 13:28:30 -0500

    refresh token in manual mfa mode

--- src/arlo_plugin/provider.py
+++ src/arlo_plugin/provider.py
@@ -57,6 +57,7 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
         self.imap = None
         self.imap_signal = None
         self.imap_skip_emails = None
+        self.manual_mfa_signal = None
         self.device_discovery_lock = asyncio.Lock()
 
         self.propagate_verbosity()
@@ -66,7 +67,9 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
             if self.mfa_strategy == "IMAP":
                 self.initialize_imap()
             else:
-                _ = self.arlo
+                arlo_init = self.arlo
+                if arlo_init is not None:
+                    self.initialize_manual_mfa_loop()
 
         asyncio.get_event_loop().call_soon(load, self)
         self.create_task(self.onDeviceEvent(ScryptedInterface.Settings.value, None))
@@ -287,6 +290,7 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
     def invalidate_arlo_client(self) -> None:
         if self._arlo is not None:
             self._arlo.Unsubscribe()
+        self.exit_manual_mfa()
         self._arlo = None
         self._arlo_mfa_code = None
         self._arlo_mfa_complete_auth = None
@@ -308,6 +312,58 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
     def propagate_transport(self) -> None:
         self.print(f"Setting plugin transport to {self.arlo_transport}")
         change_stream_class(self.arlo_transport)
+
+    def initialize_manual_mfa_loop(self) -> None:
+        self.exit_manual_mfa()
+        self.manual_mfa_signal = asyncio.Queue()
+        self.create_task(self.manual_mfa_loop())
+
+    async def manual_mfa_loop(self) -> None:
+        manual_mfa_signal = self.manual_mfa_signal
+        last_mfa = time.time()
+        self.logger.info(f"Starting manual refresh loop {id(manual_mfa_signal)}")
+        while True:
+            # continue by sleeping/waiting for a signal
+            # 60 minutes, +/- a random amount of time
+            interval = 60 * 60 + random.randint(-300, 300)
+            signal_task = asyncio.create_task(manual_mfa_signal.get())
+
+            # wait until either we receive a signal or the refresh interval expires
+            sleep_task = asyncio.create_task(asyncio.sleep(interval))
+            done, pending = await asyncio.wait([signal_task, sleep_task], return_when=asyncio.FIRST_COMPLETED)
+            for task in pending:
+                task.cancel()
+
+            done_task = done.pop()
+            if done_task is signal_task and done_task.result() is None:
+                # exit signal received
+                self.logger.info(f"Exiting manual refresh loop {id(manual_mfa_signal)}")
+                return
+
+            if time.time() - last_mfa > 14 * 24 * 60 * 60:
+                self.logger.info("Clearing cookies to force re-authentication")
+                self.storage.setItem("arlo_cookies", "")
+
+            self._arlo = None
+
+            try:
+                arlo_init = self.arlo
+            except Exception:
+                self.logger.exception("Unrecoverable login error")
+                self.logger.error("Will request a plugin restart")
+                await scrypted_sdk.deviceManager.requestRestart()
+                return
+
+            if arlo_init is None:
+                self.logger.info("Manual MFA token required")
+                return
+            else:
+                _ = self.arlo
+
+    def exit_manual_mfa(self) -> None:
+        if self.manual_mfa_signal:
+            self.manual_mfa_signal.put_nowait(None)
+        self.manual_mfa_signal = None
 
     def initialize_imap(self, try_count=1) -> None:
         if not self.imap_mfa_host or not self.imap_mfa_port or \
@@ -489,7 +545,8 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
             signal_task = asyncio.create_task(imap_signal.get())
 
             # wait until either we receive a signal or the refresh interval expires
-            done, pending = await asyncio.wait([signal_task, asyncio.sleep(interval)], return_when=asyncio.FIRST_COMPLETED)
+            sleep_task = asyncio.create_task(asyncio.sleep(interval))
+            done, pending = await asyncio.wait([signal_task, sleep_task], return_when=asyncio.FIRST_COMPLETED)
             for task in pending:
                 task.cancel()
 
@@ -644,6 +701,7 @@ class ArloProvider(ScryptedDeviceBase, Settings, DeviceProvider, ScryptedDeviceL
         skip_arlo_client = False
         if key == "arlo_mfa_code":
             self._arlo_mfa_code = value
+            self.initialize_manual_mfa_loop()
         elif key == "force_reauth":
             # force arlo client to be invalidated and reloaded
             self.invalidate_arlo_client()
